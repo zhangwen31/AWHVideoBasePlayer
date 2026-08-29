da331459f51d3cc33c166bc753173604c8edecf2
王恒 <1066026709@qq.com>
2026-08-29 15:31:56 +0800

    修改配置

---
+++ AWHVideoBasePlayer.framework/Headers/AWHVBPIJKFFMoviePlayerController.h
@@ -0,0 +1,18 @@
+//
+//  AWHVBPIJKFFMoviePlayerController.h
+//  AWHVideoBasePlayer
+//
+//  Created by 王恒 on 2026/7/4.
+//
+
+#import <IJKMediaFramework/IJKMediaFramework.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AWHVBPIJKFFMoviePlayerController : IJKFFMoviePlayerController
+
+- (void)setVideoPlaybackVolume:(float)playbackVolume;
+
+@end
+
+NS_ASSUME_NONNULL_END
